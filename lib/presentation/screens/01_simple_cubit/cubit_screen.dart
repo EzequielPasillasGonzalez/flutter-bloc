08e788c339bfeb14cb6c9b036d4fd0fbddbe1f9e
Ezequiel Pasillas Gonzalez <ingepasillasg@gmail.com>
2026-04-15 21:20:16 -0600

    feat: uso de cubit

--- lib/presentation/screens/01_simple_cubit/cubit_screen.dart
+++ lib/presentation/screens/01_simple_cubit/cubit_screen.dart
@@ -1,17 +1,26 @@
+import 'package:blocs_app/config/helpers/random_generator.dart';
+import 'package:blocs_app/presentation/screens/blocs/blocs.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CubitScreen extends StatelessWidget {
   const CubitScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final usernameCubit = context.watch<UsernameCubit>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cubit'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+        child: Text(usernameCubit.state),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          usernameCubit.setUsername(RandomGenerator.getRandomName());
+        },
+        child: const Icon(Icons.refresh),
       ),
     );
   }
